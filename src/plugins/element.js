131f8befbeca59f8000d451aa8a77cfaf9604edd
HeKvn <377014982@qq.com>
2020-12-11 16:21:41 +0800

    完成登陆功能

---
+++ src/plugins/element.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// 弹框提示组件，需要挂载
+Vue.prototype.$message = Message
